7ef04d98c6505e12c71949900393b044e9a90e03
Kimoto-Norihiro <n000r111code@gmail.com>
2025-01-22 16:25:18 +0900

    add tavily search

--- backend/rag-sample.py
+++ backend/rag-sample.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 from langchain.schema.runnable import Runnable
 from langchain_chroma import Chroma
+from langchain_community.retrievers import TavilySearchAPIRetriever
 from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.retrievers import BaseRetriever
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
@@ -11,24 +13,22 @@ from langchain_unstructured import UnstructuredLoader
 
 load_dotenv()
 
-# RAGに追加するPDFファイルを指定
-loader = UnstructuredLoader(
-    file_path="./documents/SPGP-SAEA.pdf",
-)
+retriever_type = "tavily"
 
-raw_docs = loader.load()
-
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-
-docs = text_splitter.split_documents(raw_docs)
-
-filtered_docs = filter_complex_metadata(docs)
-
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
-db = Chroma.from_documents(filtered_docs, embeddings)
-
-retreiver = db.as_retriever()
+retriever: BaseRetriever
+if retriever_type == "tavily":
+    retriever = TavilySearchAPIRetriever(k=3)
+else:
+    loader = UnstructuredLoader(
+        file_path="./documents/SPGP-SAEA.pdf",
+    )
+    raw_docs = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(raw_docs)
+    filtered_docs = filter_complex_metadata(docs)
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    db = Chroma.from_documents(filtered_docs, embeddings)
+    retriever = db.as_retriever()
 
 template = '''
  以下の文脈だけを踏まえて質問に回答してください。
@@ -40,18 +40,17 @@ template = '''
 '''
 
 prompt = PromptTemplate(template=template, input_variables=["context", "question"])
-
 model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 chain: Runnable = (
-    {"context": retreiver, "question": RunnablePassthrough()}
+    {"context": retriever, "question": RunnablePassthrough()}
     | prompt
     | model
     | StrOutputParser()
 )
 
 # RAGへの質問
-query = "What is SPGP-SAEA?"
+query = "これはWeb検索をしていますか?"
 
 output = chain.invoke(query)
 print(output)
