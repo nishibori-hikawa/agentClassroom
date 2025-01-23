38f82ddfa05be78248e9a6fed710b5a8bcb9f60b
Kimoto-Norihiro <n000r111code@gmail.com>
2025-01-24 01:09:05 +0900

    fix pdf loading

--- backend/rag-sample.py
+++ backend/rag-sample.py
@@ -2,38 +2,44 @@ from dotenv import load_dotenv
 from langchain.schema.runnable import Runnable
 from langchain_chroma import Chroma
 from langchain_community.retrievers import TavilySearchAPIRetriever
-from langchain_community.vectorstores.utils import filter_complex_metadata
+from langchain_community.vectorstores.utils import Document, filter_complex_metadata
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_unstructured import UnstructuredLoader
+
+from utils.pdf_loader import extract_text_from_pdf
 
 load_dotenv()
 
-retriever_type = "tavily"
 
+# PDFファイルを読み込んで、Documentオブジェクトのリストに変換する
+def pdf_to_documents(file_path: str) -> list[Document]:
+    text = extract_text_from_pdf(file_path)
+    docs = Document(page_content=text)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    split_docs = text_splitter.split_documents([docs])
+    return filter_complex_metadata(split_docs)
+
+
+retriever_type = "pdf"
 retriever: BaseRetriever
 if retriever_type == "tavily":
     retriever = TavilySearchAPIRetriever(k=3)
 else:
-    loader = UnstructuredLoader(
-        file_path="./documents/SPGP-SAEA.pdf",
-    )
-    raw_docs = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = text_splitter.split_documents(raw_docs)
-    filtered_docs = filter_complex_metadata(docs)
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    db = Chroma.from_documents(filtered_docs, embeddings)
+    if True:
+        file_path = "./documents/main.pdf"
+        filtered_docs = pdf_to_documents(file_path)
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+        db = Chroma.from_documents(filtered_docs, embeddings)
     retriever = db.as_retriever()
 
 template = '''
- 以下の文脈だけを踏まえて質問に回答してください。
+ 以下の資料だけを踏まえて質問に回答してください。
 
- 文脈: """
+ 資料: """
  {context}
  """
  質問: {question}
@@ -50,7 +56,7 @@ chain: Runnable = (
 )
 
 # RAGへの質問
-query = "これはWeb検索をしていますか?"
+query = "筆者の主張を教えてください。"
 
 output = chain.invoke(query)
 print(output)
