9f45a3b98f8853864f6c33e0ac4b3b598fe3df49
Kimoto-Norihiro <n000r111code@gmail.com>
2025-01-27 10:01:35 +0900

    impl agent base

--- backend/agent.py
+++ backend/agent.py
@@ -2,27 +2,19 @@ from pprint import pprint
 from typing import Any
 
 from dotenv import load_dotenv
-from langchain_chroma import Chroma
 from langchain_core.language_models import BaseChatModel
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.runnables import Runnable
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_google_vertexai import VertexAI
+from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph
 from pydantic import BaseModel, Field
 
-from utils import extract_text_from_pdf, text_to_documents
+from retrievers import create_pdf_retriever
 
 load_dotenv()
-
-
-def create_pdf_retriever(file_path: str) -> BaseRetriever:
-    text = extract_text_from_pdf(file_path)
-    filtered_docs = text_to_documents(text)
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    db = Chroma.from_documents(filtered_docs, embeddings)
-    return db.as_retriever()
 
 
 class State(BaseModel):
@@ -32,26 +24,12 @@ class State(BaseModel):
     critic_content: str = Field(default="", description="criticの回答内容")
 
 
-class AgentClassroom:
-    def __init__(self, llm: BaseChatModel, retriever: BaseRetriever) -> None:
-        self.graph = self._create_graph()
-        self.retriever = retriever
+class Reporter:
+    def __init__(self, retriever: BaseRetriever, llm: BaseChatModel) -> None:
         self.llm = llm
+        self.retriever = retriever
 
-    def _create_graph(self) -> StateGraph:
-        workflow = StateGraph(State)
-
-        workflow.add_node("reporter", self.reporter_node)
-        workflow.add_node("critic", self.critic_node)
-        workflow.add_edge("reporter", "critic")
-        workflow.set_entry_point("reporter")
-
-        return workflow
-
-    def reporter_node(self, state: State) -> dict[str, Any]:
-        query = state.query
-        retriever = self.retriever
-
+    def generate_report(self, query: str) -> str:
         template = '''
         あなたは国際政治演習に参加している報告担当の生徒です。
         以下の資料を元に、簡潔にレポートを作成してください。
@@ -68,23 +46,18 @@ class AgentClassroom:
         prompt = PromptTemplate(template=template, input_variables=["context", "question"])
         model = self.llm
 
-        chain: Runnable = {"context": retriever} | prompt | model | StrOutputParser()
+        chain: Runnable = {"context": self.retriever} | prompt | model | StrOutputParser()
 
-        # RAGへの質問
         output = chain.invoke(query)
-        generated_text = output
+        return output
 
-        return {
-            "query": query,
-            "current_role": "reporter",
-            "reporter_content": generated_text,
-        }
 
-    def critic_node(self, state: State) -> dict[str, Any]:
-        query = state.query
-        report_text = state.reporter_content
-        context = self.retriever.invoke(report_text)
+class Critic:
+    def __init__(self, llm: BaseChatModel, retriever: BaseRetriever) -> None:
+        self.llm = llm
+        self.retriever = retriever
 
+    def generate_critique(self, report_text: str) -> str:
         template = '''
         あなたは国際政治演習に参加している生徒で、批判的視点からの論点を抽出する専門家です。
         報告文: {report_text} を読み、資料をもとに以下の観点で論点を3つ抽出してください。
@@ -100,18 +73,57 @@ class AgentClassroom:
         各論点は簡潔に、100文字以内でまとめること。
         '''
 
+        context = self.retriever.invoke(report_text)
         prompt = PromptTemplate(template=template, input_variables=["report_text", "context"])
         model = self.llm
         chain = prompt | model | StrOutputParser()
 
         output = chain.invoke({"report_text": report_text, "context": context})
-        generated_text = output
+        return output
+
+
+class AgentClassroom:
+    def __init__(self, llm: BaseChatModel, retriever: BaseRetriever) -> None:
+        self.graph = self._create_graph()
+        self.retriever = retriever
+        self.llm = llm
+        self.reporter = Reporter(retriever, llm)
+
+    def _create_graph(self) -> StateGraph:
+        workflow = StateGraph(State)
+
+        workflow.add_node("reporter", self.reporter_node)
+        workflow.add_node("critic", self.critic_node)
+        workflow.add_edge("reporter", "critic")
+        workflow.set_entry_point("reporter")
+
+        return workflow
+
+    def reporter_node(self, state: State) -> dict[str, Any]:
+        query = state.query
+
+        reporter = self.reporter
+        generated_text = reporter.generate_report(query)
+
+        return {
+            "query": query,
+            "current_role": "reporter",
+            "reporter_content": generated_text,
+        }
+
+    def critic_node(self, state: State) -> dict[str, Any]:
+        query = state.query
+        report_text = state.reporter_content
+
+        critic = Critic(self.llm, self.retriever)
+        generated_text = critic.generate_critique(report_text)
 
         return {"query": query, "current_role": "critic", "critic_content": generated_text}
 
 
 def main():
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    # llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    llm = VertexAI(model="gemini-1.5-flash-001", temperature=0)
     retriever = create_pdf_retriever("./documents/main.pdf")
     agent = AgentClassroom(llm, retriever)
     compiled = agent.graph.compile()
